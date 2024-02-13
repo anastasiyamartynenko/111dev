6d0fea69470a641b75e35629b18e2c2e63153960
anastasiyamartynenko <a.martynenko@jusaninvest.com>
2024-02-13 13:56:37 +0600

    app

--- script/app.js
+++ script/app.js
@@ -32,26 +32,105 @@ $(document).ready(function(){
 
 //* Слайд "Первое правило инвестиций *//
 $(document).ready(function(){
-    // Показать и сделать активным block-sl-1-img при загрузке страницы
+    var autoplayInterval = 3000; // Продолжительность интервала автовоспроизведения в миллисекундах
+    var autoplayIntervalID; // Идентификатор интервала автовоспроизведения
+    var animationStartTime; // Время начала анимации индикатора выполнения
+    var progressAnimationDuration; // Продолжительность анимации индикатора выполнения
+    var remainingAnimationTime; // Оставшееся время анимации индикатора выполнения
+    var isPaused = false; // Флаг для отслеживания, если автовоспроизведение приостановлено из-за наведения мыши
+
+    function showProgress($block) {
+        $block.find(".block-sl2-progress").show(); // Показать индикатор выполнения для активного блока
+
+        // Установите продолжительность анимации индикатора выполнения, равную продолжительности интервала автозапуска
+        progressAnimationDuration = autoplayInterval;
+        $block.find(".block-sl2-progress").css("animation-duration", progressAnimationDuration + "ms");
+
+        // Установить время начала анимации
+        animationStartTime = performance.now();
+    }
+
+    function hideProgress($block) {
+        $block.find(".block-sl2-progress").hide(); // Скрыть индикатор выполнения для неактивного блока
+    }
+
     $(".block-sl-5-img").show();
-    $(".block-sl-6-img, .block-sl-7-img").hide(); // Скрыть остальные изображения
+    $(".block-sl-6-img, .block-sl-7-img").hide();
     $(".block-sl-5").addClass("active");
+    showProgress($(".block-sl-5")); // Показывать индикатор выполнения для первого блока при загрузке страницы
 
-    // Обработчики событий hover для блоков
-    $(".block-sl2").hover(function(){
-        $(".block-sl2").removeClass("active"); // Удалить класс active у всех блоков
-        $(this).addClass("active"); // Добавить класс active блоку, на который навели курсор
+    function autoPlay() {
+        if (!isPaused) {
+            var currentActive = $(".block-sl2.active").index();
+            var nextActive = (currentActive + 1) % $(".block-sl2").length;
+
+            $(".block-sl2").removeClass("active");
+            $(".block-sl2").eq(nextActive).addClass("active");
+
+            $(".block-sl-5-img, .block-sl-6-img, .block-sl-7-img").hide();
+            $(".block-sl-" + (nextActive + 5) + "-img").show();
+
+            var $nextBlock = $(".block-sl-" + (nextActive + 5));
+            showProgress($nextBlock);
+
+            // Скрыть индикатор выполнения предыдущего блока
+            var $prevBlock = $(".block-sl-" + (currentActive + 5));
+            hideProgress($prevBlock);
+        }
+    }
+
+    autoplayIntervalID = setInterval(autoPlay, autoplayInterval);
+
+    $(".block-sl2").click(function() {
+        clearInterval(autoplayIntervalID);
+        $(".block-sl2").removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $(".block-sl-5-img, .block-sl-6-img, .block-sl-7-img").hide();
+        $(".block-sl-" + (index + 5) + "-img").show();
+        showProgress($(this));
+        autoplayIntervalID = setInterval(autoPlay, autoplayInterval);
     });
 
-    $(".block-sl-6").hover(function(){
-        $(".block-sl-6-img").show();
-        $(".block-sl-5-img, .block-sl-7-img").hide();
+    var progressAnimationTimer;
+    
+    $(".block-sl2").mouseenter(function() {
+        if (!$(this).hasClass("active")) {
+            // Если блок не активен, то просто выходим из обработчика события
+            return;
+        }
+    
+        isPaused = true;
+        clearInterval(autoplayIntervalID); // Остановить автовоспроизведение при наведении курсора на активный блок
+        progressAnimationDuration = parseInt($(this).find(".block-sl2-progress").css("animation-duration")); // Сохранить текущую продолжительность анимации индикатора выполнения
+        $(this).find(".block-sl2-progress").css("animation-play-state", "paused"); // Приостановить анимацию индикатора выполнения
+    }).mouseleave(function() {
+        if (!$(this).hasClass("active")) {
+            // Если блок не активен, то просто выходим из обработчика события
+            return;
+        }
+    
+        isPaused = false;
+        autoplayIntervalID = setInterval(autoPlay, autoplayInterval); // Возобновить автовоспроизведение, когда мышь покидает активный блок
+    
+        // Рассчитать оставшееся время анимации индикатора выполнения
+        remainingAnimationTime = progressAnimationDuration - (performance.now() - animationStartTime);
+        if (remainingAnimationTime > 0) {
+            // Установить оставшуюся продолжительность анимации
+            $(this).find(".block-sl2-progress").css("animation-duration", remainingAnimationTime + "ms");
+            // Установить время начала анимации на текущее время минус оставшееся время анимации
+            animationStartTime = performance.now() - remainingAnimationTime;
+        }
+    
+        $(this).find(".block-sl2-progress").css("animation-play-state", "running"); // Возобновить анимацию индикатора выполнения
+        
+        // Запустить автовоспроизведение после возобновления анимации
+        autoplayIntervalID = setInterval(autoPlay, autoplayInterval);
     });
-    $(".block-sl-7").hover(function(){
-        $(".block-sl-7-img").show();
-        $(".block-sl-5-img, .block-sl-6-img").hide();
-    });   
 });
+
+
+
 
 
 Highcharts.setOptions({
@@ -104,3 +183,4 @@ Highcharts.setOptions({
           },
       ]
   });
+
